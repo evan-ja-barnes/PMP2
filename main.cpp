847f4e72bdc34ead0c2228ad0f0b480bd071ecf2
Evan Barnes <ejb3831@rit.edu>
2020-08-30 02:18:28 -0400

    added function to retrive active window, search the database and autotype the password

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+/*
+A simple pass word manager
+Author: Evan Barnes
+Date: 8/22/2020
+*/
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -5,23 +10,19 @@
 #include <utility>
 #include <stdexcept>
 #include <sstream>
-
+#include <windows.h>
 #include <iostream> 
 
 
-#include <sqlite3.h>
-  
 
 
 using namespace std;
 
 vector<vector<string>> readCSV(string filename){
-    
-    
-    
+    string PATH = "C:/Users/Stran/Downloads/Docs/";
     int numRows;
     int numCols;
-    ifstream dBImport(filename); 
+    ifstream dBImport(PATH + filename); 
     if(!dBImport.is_open()) throw runtime_error("could not open file");
     string line, colName;
     
@@ -56,9 +57,22 @@ vector<vector<string>> readCSV(string filename){
             getline(dBImport, line);
             stringstream ss(line);
             for(int j = 0; j<numCols; j ++){
+                
                 string line2;
                 getline(ss, line2, ',');
-                
+                int sz = line2.size()-1;
+                if(sz>0){
+                if(line2.at(sz) =='"'){
+                    
+                    if(outVect1[i][j-1].at(0) == '"'){
+
+                        string st1 = (outVect1[i][j-1] + ',' + line2);
+                        string st2 = st1.substr(1, st1.size()-2);
+                        outVect1[i][j-1] =st2;
+                        continue;
+                    }
+                }
+                }
                 outVect1[i][j] = line2;
                 
                 colCT++;
@@ -79,15 +93,58 @@ vector<vector<string>> readCSV(string filename){
     dBImport.close();
     return outVect;
 }
-int main(){
-    vector<vector<string>> pp = readCSV("DB.csv");
-    //cout << pp[0][0];
-    for(vector<string> vec : pp){
-        for(string st : vec){
-            cout << st << "; ";
+class PMP{
+    public:
+
+    vector<vector<string>> currentDB;
+    
+    PMP(string dbName){
+        currentDB = readCSV(dbName);
+        
+    }
+    string getAssPass(string inpass){
+        for(auto line : currentDB){
+            if(line[0] == inpass){
+                return line[1];
+            }
+        }
+        return "";
+    }
+    void typeOut(string typer){
+        vector<INPUT> vec;
+        for(auto ch : typer){
+            //SendInput(ch);
+            INPUT input = { 0 };
+            input.type = INPUT_KEYBOARD;
+            input.ki.dwFlags = KEYEVENTF_UNICODE;
+            input.ki.wScan = ch;
+            vec.push_back(input);
+            input.ki.dwFlags |= KEYEVENTF_KEYUP;
+            vec.push_back(input);
+
             
         }
-        cout << "/n";
+        
+        SendInput(vec.size(), vec.data(), sizeof(INPUT));
     }
+    string getActWind(){
+        HWND top = GetForegroundWindow();   
+        char charray[255];
+        string finTop;
+        GetWindowText(top,charray,sizeof(charray));
+        string st = charray;
+        stringstream ss(st);
+        
+        while(getline(ss, finTop, '-')){}
+        finTop = finTop.substr(1, sizeof(finTop)-1);
+        cout << finTop;
+        return finTop;
+    }
+    
+};
+
+int main(){
+    PMP daPMP("db.csv");
+    string activeWindow = daPMP.getActWind();
     return 0;
 }   
